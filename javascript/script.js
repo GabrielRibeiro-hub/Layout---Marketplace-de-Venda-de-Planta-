d423ab158bdbd090cab3a3027fbb5d12a1f7d4c3
Gabriel Ribeiro <gabrielribeiro.gr82@gmail.com>
2022-06-27 07:48:26 -0300

    Novas alteraçoes

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,9 @@
-console.log("testando")
+
+var button=document.getElementById("ativa");
+button.addEventListener("click", function(){
+
+    var img12 = document.getElementById("img-12");
+
+    img12.classList.toggle("hide")
+
+ })
